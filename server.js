8359b0fef8d0965855d1fc4c64f33209591d9934
Alex Vogt <alexvogt18@gmail.com>
2021-06-24 17:16:34 -0500

    Most bugs fixed

--- server.js
+++ server.js
@@ -13,22 +13,22 @@ const client = jwksClient({
 });
 
 // comes from jsonwebtoken documentation
-// function getKey(header, callback){
-//   client.getSigningKey(header.kid, function(err, key) {
-//     var signingKey = key.publicKey || key.rsaPublicKey;
-//     callback(null, signingKey);
-//   });
-// }
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/books', {useNewUrlParser: true, useUnifiedTopology: true});
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/books', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// // database error warnings from the quickstart guide
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   // we're connected!
-//   console.log('successfully connected to mongo');
-// });
+// database error warnings from the quickstart guide
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+  console.log('successfully connected to mongo');
+});
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
@@ -41,7 +41,7 @@ let firstBook = new Book({
   status: 'available',
   email: 'alexvogt18@gmail.com'
 });
-firstBook.save( (err, bookFromMongo) => {
+firstBook.save((err, bookFromMongo) => {
   console.log(bookFromMongo);
 });
 let secondBook = new Book({
@@ -50,16 +50,16 @@ let secondBook = new Book({
   status: 'available',
   email: 'alexvogt18@gmail.com'
 });
-secondBook.save( (err, bookFromMongo) => {
+secondBook.save((err, bookFromMongo) => {
   console.log(bookFromMongo);
 });
-let thirdBook = new Book ({
+let thirdBook = new Book({
   name: 'harry potter and the sorcers stone',
   description: 'fantasy',
   status: 'available',
   email: 'alexvogt18@gmail.com'
 });
-thirdBook.save( (err, bookFromMongo) => {
+thirdBook.save((err, bookFromMongo) => {
   console.log(bookFromMongo);
 });
 
@@ -69,21 +69,22 @@ app.post('/booksTest', (req, res) => {
 });
 
 app.post('/books', (req, res) => {
-    const token = req.headers.authorization.split(' ')[1];
-    console.log(token);
-    jwt.verify(token, getKey, {}, function(err, user) {
-      if(err) {
-        res.status(500).send('invalid token');
-        console.log(user);
-      } else {
-        const newBook = new Book({
-          name: req.body.name,
-          description: req.body.description,
-          status: req.body.status,
-          email: user.email
-        });
-      }
-    });
+  const token = req.headers.authorization.split(' ')[1];
+  console.log(token);
+  jwt.verify(token, getKey, {}, function (err, user) {
+    if (err) {
+      res.status(500).send('invalid token');
+      console.log(user);
+    } else {
+      const newBook = new Book({
+        name: req.body.name,
+        description: req.body.description,
+        status: req.body.status,
+        email: user.email
+      });
+    }
+  });
+});
 
 app.get('/bookTest', (req, res) => {
   Book.find({}, (err, bookData) => {
@@ -92,21 +93,21 @@ app.get('/bookTest', (req, res) => {
   });
 });
 
-// app.get('/books', (req, res) => {
-//   const token = req.headers.authorization.split(' ')[1];
-//   console.log(token);
-//   jwt.verify(token, getKey, {}, function(err, user) {
-//     if(err) {
-//       res.status(500).send('invalid token');
-//       console.log(user);
-//     } else {
-//       let userEmail = user.email;
-//       Book.find({email: userEmail}, (err, books) => {
-//         console.log(books);
-//         res.send(books);
-//       });
-//     }
-//   });
-// });
+app.get('/books', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  console.log(token);
+  jwt.verify(token, getKey, {}, function (err, user) {
+    if (err) {
+      res.status(500).send('invalid token');
+      console.log(user);
+    } else {
+      let userEmail = user.email;
+      Book.find({ email: userEmail }, (err, books) => {
+        console.log(books);
+        res.send(books);
+      });
+    }
+  });
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
